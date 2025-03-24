09c702f03017231a801bb7a0ac175b889a12dc1e
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-24 14:20:13 +0600

    add-resolvers

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,22 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `;
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
