4bd95e61d51899f6d24f48d96735af07cdee9335
Kamrulthedev <kamrulthedevR@gmail.com>
2025-04-03 15:47:14 +0600

    add-dda

--- src/db.ts
+++ src/db.ts
@@ -353,7 +353,8 @@ const reviews = [
         "id" : "33aka445Uka4344049303020k044",
         "review" : "Kamrul Hassan 3",
         "raiting" : 4.05,
-        "date" : "2025-1-23"
+        "date" : "2025-1-23",
+        "prodctId" : "eoejlk484893"
     }
 ]
 
