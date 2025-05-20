e75466eca72d35992b94304586af4f26f66cdc40
Kamrulthedev <kamrulthedevR@gmail.com>
2025-05-20 18:06:19 +0600

    market-commit

--- src/db.ts
+++ src/db.ts
@@ -180,7 +180,9 @@ const products = [
         "categoryId": "97b84bd6-c11b-435f-8943-17dc97694451"
     },
     {
-        "id": "6b1e0518-8e0d-48de-9c99-61251900ec187"
+        "id": "6b1e0518-8e0d-48de-9c99-61251900ec187",
+        "name":"Computer Market",
+        
     }
 ];
 
