495510eda84cb6f0f7bc8de055eb16b884a2c2ab
Kamrulthedev <kamrulthedevR@gmail.com>
2025-05-25 00:17:41 +0600

    date-id-commit

--- src/db.ts
+++ src/db.ts
@@ -237,6 +237,9 @@ const categories = [
     {
         "id":"97b84bd6-c11b-435f-8943-17dc97694450",
         "name":"Blabs"
+    },
+    {
+        "id":"2a089dca-d882-4305-9e25-d1dfeb93fd3"
     }
 ];
 
