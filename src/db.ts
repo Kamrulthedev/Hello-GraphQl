0f95e0c60d85c2b4d65ec2af9b5972f53540b153
Kamrulthedev <kamrulthedevR@gmail.com>
2025-06-04 00:23:31 +0600

    Boxing T-Shirt-image

--- src/db.ts
+++ src/db.ts
@@ -42,7 +42,7 @@ const products = [
     {
         "id": "2d1fd6b6-2c2b-423d-a8aa-15fc5bd723fg",
         "name": "Boxing T-Shirt",
-        "image": "running-shoes-image.jpg",
+        "image": "boxing-t-shirt-image.jpg",
         "description": "Comfortable running shoes for your active lifestyle.",
         "price": 49.99,
         "quantity": 50,
