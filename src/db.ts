dfa058bf767d7bb3606ec78bce9fb12987975f4c
Kamrulthedev <kamrulthedevR@gmail.com>
2025-06-09 23:34:11 +0600

    update-andorid-phone

--- src/db.ts
+++ src/db.ts
@@ -248,7 +248,7 @@ const products = [
         "id":"6b1e0518-8e0d-48de-9c99-61251900ec267",
         "name":"Anderoid Phone",
         "image":"android-phone.image"
-
+        "description":"anderoid phone for your daily use"
     }
     // add more products as needed
 ];
