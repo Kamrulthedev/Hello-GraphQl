966b6f38f4553ab252b83fc3c7e4c24f53ad477d
Kamrulthedev <kamrulthedevR@gmail.com>
2025-07-31 09:55:37 +0600

    return-reviews-data

--- src/gql/resolvers/index.ts
+++ src/gql/resolvers/index.ts
@@ -18,7 +18,14 @@ export const resolvers = {
         },
         reviews:(parent, args, context) =>{
            console.log("parent", parent);
+           return db.reviews.filter(reviews => reviews.productId === parent.id) || [];
+
         }
+        // reviews:(parent, args, context) =>{
+        //    console.log("parent", parent);
+        //    const reviews = db.reviews.filter(review => review.productId);
+
+        //    return reviews.length > 0 ? reviews : "No Reviews Kamrul Bhai!!🤣🤣" }
     },
     Category:{
         products : (parent, args, context) =>{
